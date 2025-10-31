1df7449c046e2c18a9c211bed153b823bfa9b95c
Rodolph <rodolphcorp@gmail.com>
2025-10-31 15:20:12 +0100

    update loginpage component

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,34 @@ function App() {
 
   // comportements
 
-  // affichage
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Bienvenu ${prenom}`);
+    // Réinitialiser le formulaire si nécessaire
+    setPrenom("");
+  };
 
+  // affichage
   return (
     <div>
-      <h1> Bienvue chez nous {prenom}</h1>
-      <h3> Connectez-vous</h3>
+      <h1>Hello {prenom}</h1>
+
+      <form onSubmit={handleSubmit}>
+        <h2>Formulaire de contact</h2>
+        <input
+          type="text"
+          placeholder="Entrez votre Prénom"
+          value={prenom}
+          onChange={(e) => setPrenom(e.target.value)}
+          required
+        />
+        <button type="submit">Envoyer</button>
+      </form>
+
+      <br />
+      <h3>Connectez-vous</h3>
     </div>
   );
 }
+
 export default App;
--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,6 +1,38 @@
 import React from 'react'
+import React, { useState } from 'react'
+
 
 export default function LoginPage() {
-  return (<div>LoginPage</div>
-  )
+  // states
+  const [prenom, setPrenom] = useState("Rod");
+
+  // comportements
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Bienvenu ${prenom}`);
+    // Réinitialiser le formulaire si nécessaire
+    setPrenom("");
+  };
+
+  // affichage
+  return (
+    <div>
+      <h1>Hello {prenom}</h1>
+
+      <form onSubmit={handleSubmit}>
+        <h2>Formulaire de contact</h2>
+        <input
+          type="text"
+          placeholder="Entrez votre Prénom"
+          value={prenom}
+          onChange={(e) => setPrenom(e.target.value)}
+          required
+        />
+        <button type="submit">Envoyer</button>
+      </form>
+
+      <br />
+      <h3>Connectez-vous</h3>
+    </div>
+  );
 }
