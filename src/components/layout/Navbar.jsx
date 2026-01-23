f4f0d81198ae2f69bb00c01647a11e425500d2de
Rodolph <rodolphcorp@gmail.com>
2026-01-23 22:25:45 +0100

    Maj navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { theme } from "../../../index";
-import { useParams } from "react-router-dom";
+import { theme } from "../../index";
+import { useParams, Link } from "react-router-dom";
 import { MdAccountCircle } from "react-icons/md";
 
 
@@ -12,16 +12,24 @@ export default function Navbar() {
   return (
     <NavbarStyled>
       <div className="navbar">
-        <img
-          className="logo-burger"
-          src="/images/f03-logo-orange.png"
-          alt="logo orange burger"
-        />{" "}
-        BURGER
+        <div className="flex-logo">
+          <h3>
+            CRAZEE{" "}
+            <img
+              className="logo-burger"
+              src="/images/f03-logo-orange.png"
+              alt="logo orange burger"
+            />{" "}
+            BURGER
+          </h3>
+        </div>
         <div className="flex-text-nav">
-          <p>Hey {prenom}</p>
+          <p>
+            Hey, <span className="username">{prenom}</span>{" "}
+          </p>
           <h4>
-            se déconnecter <MdAccountCircle className="icon" />
+            <Link to="/">se déconnecter</Link>
+            <MdAccountCircle className="icon" size="22px" />
           </h4>
         </div>
       </div>
@@ -30,20 +38,85 @@ export default function Navbar() {
 }
 
 const NavbarStyled = styled.nav`
+  .flex-logo {
+    display: flex;
+    justify-self: center;
+    align-items: center;
+  }
+  .navbar h3 {
+    font-family: Amatic SC;
+    font-weight: 700;
+    font-style: Bold;
+    font-size: 36px;
+    line-height: 46px;
+    letter-spacing: 1.5px;
+    text-align: center;
+    vertical-align: middle;
+    text-transform: uppercase;
+    color: #ffa01b;
+    display: flex;
+    align-items: center;
+  }
+  .navbar h4 a {
+    font-family: Open Sans;
+    font-weight: 400;
+    font-style: Regular;
+    font-size: 10px;
+    line-height: 14px;
+    letter-spacing: 0%;
+    text-align: right;
+    vertical-align: middle;
+    text-decoration: none; /* Enlève le soulignement */
+    color: inherit; /* Hérite la couleur du h4 */
+  }
 
+  .navbar h4 {
+    font-family: Open Sans;
+    font-weight: 400;
+    font-style: Regular;
+    font-size: 10px;
+    line-height: 14px;
+    letter-spacing: 0%;
+    text-align: right;
+    vertical-align: middle;
+    margin-top: -7px;
+  }
+
+  .navbar p {
+    display: flex;
+    font-family: Open Sans;
+    font-weight: 400;
+    font-style: Regular;
+    font-size: 16px;
+    line-height: 22px;
+    letter-spacing: 0%;
+    text-align: right;
+    vertical-align: middle;
+  }
 
   .logo-burger {
-    width: 140px;
-    height: 130px;
-    margin: 0 10px;
+    width: 80px;
+    height: auto;
   }
   .navbar {
     display: flex;
-    background-color: white ;
-
+    background-color: white;
+    box-shadow: 0px 8px 20px 8px #00000033 inset;
+    align-items: center;
+    justify-content: space-between;
+    padding: 15px 50px;
   }
   .flex-text-nav {
     display: flex;
     flex-direction: column;
+  }
+  .username {
+    font-family: Open Sans;
+    font-weight: 800;
+    font-style: Regular;
+    font-size: 16px;
+    line-height: 22px;
+    letter-spacing: 0%;
+    color: #ffa01b;
   }
 `;
--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,19 +1,13 @@
-import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
-import Navbar from "src/components/layout/Navbar";
+import Navbar from "../../layout/Navbar";
 
 export default function OrderPage() {
   // states
-  const{prenom} = useParams();
 
 
 return (
   <div>
-    <Navbar>
-      <h1>Bonjour {prenom} </h1>
+    <Navbar/>
 
-      <Link to="/">Déconnexion</Link>
-    </Navbar>
   </div>
 );
 }
